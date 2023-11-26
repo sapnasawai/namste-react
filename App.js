d64fa86ff24627589cfe4c8c4ce4db07a43e762b
Sapna <sapnasawai1999@gmail.com>
2023-11-26 20:36:26 +0530

    Day2

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
-const heading = React.createElement("h1", {}, "Hello world from React");
-const root =  ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+// const heading = React.createElement("h1", {}, "Hello world from React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("div", {}, "I'm an h1 tag"),
+    React.createElement("div", {}, "I'm an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("div", {}, "I'm an h1 tag"),
+    React.createElement("div", {}, "I'm an h2 tag"),
+  ]),
+]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ README.md
@@ -0,0 +1,20 @@
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm
+- Caching
+- Image Optimization
+- Minification
+- Building
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browser
+- Error Handling
+- Can Host app on HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
+
+# Read parcel's docs parcel.org
