b65260fe90dc44ccb902cb102e5cd2391dafd711
Sapna <sapnasawai1999@gmail.com>
2023-12-02 09:28:06 +0530

    Episode 3

--- App.js
+++ App.js
@@ -3,18 +3,30 @@
 // root.render(heading);
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("div", {}, "I'm an h1 tag"),
-    React.createElement("div", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("div", {}, "I'm an h1 tag"),
-    React.createElement("div", {}, "I'm an h2 tag"),
-  ]),
-]);
 
-console.log(parent);
+const heading = React.createElement("h1", { id: "heading" }, "Namste React 🚀");
+console.log(heading);
+
+// JS Engine doesn't understand JSX(Transpiled before it reches browser) = Parcel -> Babel
+
+// JSX(transpiled) => React.createElement => React Element(JS- object) => HTMLElement Render
+// React Element
+const jsxheading = <h1>Namste React Using JSX 🚀</h1>;
+
+const TitleComponent = () => {
+  <h1 className="head" tabIndex={5}>
+    Namste React Using JSX 🚀
+  </h1>;
+};
+
+// React Component
+const HeadingCompontent = () => {
+  <div id="container">
+    <TitleComponent />
+    <h1>Namste React Functional Component</h1>
+  </div>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingCompontent />);
