696f2d69fc4557afe86aa3c7fc8b97a08943b9fb
Sapna <sapnasawai1999@gmail.com>
2024-01-18 10:53:13 +0530

    EP03 laying foundation

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
-// const heading = React.createElement('h1',{id:"heading"},'Hello world from React')
 import React from "react"
 import  ReactDOM  from "react-dom/client"
 
-const parent = React.createElement('div', {id:"parent"}, 
-[
-    React.createElement('div', {id:"child"}, 
-[
-    React.createElement('h1', {}, 'I am h1 tag'),
-    React.createElement('h2', {}, 'I am h2 tag')
-] 
-),
-React.createElement('div', {id:"child"}, 
-[
-    React.createElement('h1', {}, 'I am h1 tag'),
-    React.createElement('h2', {}, 'I am h2 tag')
-] 
-)
-])
+//React.createElement => object => HTMLElement(after render)
+
+const heading = React.createElement('h1',{id:"heading"}, 'Namste React')
+
+// JSX => HTML or XML like syntax
+
+const jsxHeading = <h1 id="heading">Namste React using JSX</h1>
+
+// both are react element: jsxHeading react elem using jsx
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent)
+root.render(heading)
