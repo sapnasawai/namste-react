a6b1008898d7549c65f2079e9d56fd7086008297
Sapna <sapnasawai1999@gmail.com>
2024-01-29 10:15:00 +0530

    Ep 04

--- App.js
+++ App.js
@@ -1,15 +1,56 @@
 import React from "react"
 import  ReactDOM  from "react-dom/client"
 
-//React.createElement => object => HTMLElement(after render)
+const Header = () => {
+    return(
+        <div className="headerContainer">
+           <div className="imageContainer">
+            <img className="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnn4m1Ap6mClU9fZq6esWlK8E6vIvB5mKk2T9XCxzs4QhA5LeOXh3EVy_HrM1_lgXDTxY&usqp=CAU"/>
+           </div>
+           <div className="navItems">
+             <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Cart</li>
+             </ul>
+           </div>
+        </div>
+    )
+}
 
-const heading = React.createElement('h1',{id:"heading"}, 'Namste React')
+const Search = () => {
+    return(
+        <div className="searchContainer">
+          <div className="searchBar">
+           <input placeholder="Search"/>
+          </div>
+          <button>Search</button>
+        </div>
+    )
+}
 
-// JSX => HTML or XML like syntax
+const RestaurantCard = () => {
+return(
+    <div className="cardContainer">
+      <img className="restroImage" src=""/>
+    </div>
+)
+}
 
-const jsxHeading = <h1 id="heading">Namste React using JSX</h1>
+const Body = () => {
+return(
+    <Search/>
+)
+}
 
-// both are react element: jsxHeading react elem using jsx
-
+const AppLayout = () => {
+return(
+    <div className="app">
+    <Header/>
+    <Body/>
+    </div>
+)
+}
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+root.render(<AppLayout/>)
